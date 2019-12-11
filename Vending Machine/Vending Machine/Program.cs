06719d93ae9e872fbf2194aacb83409c61c71e86
Ghiurcuta Andrei <ghiurcutaandrei@gmail.com>
2019-12-11 16:50:47 +0200

    Now we display products using Collections

--- Vending Machine/Vending Machine/Program.cs
+++ Vending Machine/Vending Machine/Program.cs
@@ -1,12 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Vending_Machine
-{
-    class Product
+{   //  10/10 
+    public class Product
     {
         private string name;
         private double price;
         private int stock;
+        
+        public Product(string name,double price,int stock)
+        {
+            this.name = name;
+            this.price = price;
+            this.stock = stock;
+        }
         public string Name
         {
             get { return name; }
@@ -24,60 +32,37 @@ namespace Vending_Machine
         }
     }
     class Program
-    {
-        public static void Display(Product[] products)
+    {   
+        
+
+        //      10/10
+        public static void Display(List<Product> products)
         {
             Console.WriteLine("0.Exit");
-            for (int i = 0; i < 8; i++)
+            for (int i = 0; i < products.Count; i++)
             {
                 Console.WriteLine((i + 1).ToString() + "." + products[i].Name + "........." + products[i].Price);
             }
 
         }
+        //      10/10
+        public static void AddProducts(List<Product> products)
+        {
+            products.Add(new Product("Coca Cola", 4.5, 30));
+            products.Add(new Product("Sprite", 4.5, 10));
+            products.Add(new Product("Fanta", 4.5, 10));
+            products.Add(new Product("Snickers", 2.5, 10));
+            products.Add(new Product("Mars", 2.5, 10));
+            products.Add(new Product("Twix", 3.5, 10));
+            products.Add(new Product("Viva", 6.5, 5));
+            products.Add(new Product("Red Bull", 5.0, 15));
+        }
+        //      8/10
         public static void Main()
         {
-            int numberOfProducts = 8;
-            Product[] products = new Product[numberOfProducts];
-            products[0] = new Product();
-            products[0].Name="Coca Cola";
-            products[0].Price = 4.5;
-            products[0].Stock = 30;
+            List<Product> products = new List<Product>();
 
-            products[1] = new Product();
-            products[1].Name = "Sprite";
-            products[1].Price = 4.5;
-            products[1].Stock = 10;
-
-            products[2] = new Product();
-            products[2].Name = "Fanta";
-            products[2].Price = 4.5;
-            products[2].Stock = 10;
-
-            products[3] = new Product();
-            products[3].Name = "Snickers";
-            products[3].Price = 2.5;
-            products[3].Stock = 10;
-
-            products[4] = new Product();
-            products[4].Name = "Mars";
-            products[4].Price = 2.5;
-            products[4].Stock = 10;
-
-            products[5] = new Product();
-            products[5].Name = "Twix";
-            products[5].Price = 3.5;
-            products[5].Stock = 10;
-
-            products[6] = new Product();
-            products[6].Name = "Viva";
-            products[6].Price = 6.5;
-            products[6].Stock = 5;
-
-            products[7] = new Product();
-            products[7].Name = "Red Bull";
-            products[7].Price = 5.0;
-            products[7].Stock = 15;
-
+            AddProducts(products);
             Display(products);
 
 
