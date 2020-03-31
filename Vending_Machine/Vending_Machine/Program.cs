4e1b5fa8c518f26310a050c57388acf834f3756b
Ghiurcuta Andrei <ghiurcutaandrei@gmail.com>
2020-03-31 15:26:17 +0300

    3 Layered Architecture Done

---
+++ Vending_Machine/Vending_Machine/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using Vending_Machine_Controller;
+using Vending_Machine_Repo;
+
+
+namespace Vending_Machine_UI
+{
+
+    public class Program
+    {
+        public static void Main()
+        {
+            Repository repo = new Repository();
+            Services service = new Services(repo);
+            UI ui = new UI(service);
+            ui.MainMenu();
+        }
+    }
+}
